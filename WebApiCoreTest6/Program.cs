ab0cb69c4fb80af0e677c2cdade4926da71b8346
adwardlxm <hahalxming@163.com>
2016-08-19 18:03:45 +0800

    添加DI示例

---
+++ WebApiCoreTest6/Program.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using System.IO;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseStartup<Startup>()
+                .UseUrls("http://localhost:8888/")
+                .Build();
+
+            host.Run();
+        }
+    }
+}
